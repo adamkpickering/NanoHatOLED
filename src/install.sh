7d046bc4982ac03b28278d7c0af2b19aca8928ad
Adam Pickering <adamkpickering@gmail.com>
2017-12-08 15:00:44 -0700

    Updated iperf3 install procedure to install more recent version, which eliminated a bug with speed_test.py

--- src/install.sh
+++ src/install.sh
@@ -6,6 +6,19 @@ if [ $(id -u) -ne 0 ]; then
     exit 1
 fi
 
+PREV_DIR=$PWD
+
 echo 'Installing iperf3...'
+sudo apt remove iperf -y
+sudo apt remove iperf3 -y
+wget -P /tmp/ http://downloads.es.net/pub/iperf/iperf-3.1.7.tar.gz
+cd /tmp
+tar xvf iperf-3.1.7.tar.gz
+cd iperf-3.1.7
+./configure
+make
+sudo make install
 yes | sudo pip install iperf3
 echo 'Success.'
+
+cd $PREV_DIR
