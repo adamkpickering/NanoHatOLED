8c1bb3993ed308646f240148e6ff0fbf20415364
Adam Pickering <adamkpickering@gmail.com>
2017-12-13 23:48:10 +0000

    Figured out methods common to every page

--- src/speed_test.py
+++ src/speed_test.py
@@ -17,13 +17,16 @@ import iperf3
 class genericPage(object):
     """Defines the basic page object and the methods it should have."""
 
-    def __init__(self, nanohat):
-        self.nanohat = nanohat
-        self.data = {}
-        self.state = 0
+    def receive_signal(self, signal):
+        print("method receive_signal not defined")
 
-    def update_data(self):
-        print("method update_data not defined")
+    @if_free
+    def display(self):
+        print("method display not defined")
+
+    @if_free
+    def change_page(self, page):
+        page = page(*args, **kwargs)
 
     def update_state(self):
         print("method update_state not defined")
@@ -31,19 +34,12 @@ class genericPage(object):
     def update_page(self):
         print("method update_page not defined")
 
-    def display_page(self):
-        print("method display_page not defined")
-
-    def receive_signal(self, signal):
-        print("method receive_signal not defined")
-
 
 class mainPage(genericPage):
     """Object that represents the main page, which shows general system
        information."""
 
     def __init__(self, nanohat):
-        self.nanohat = nanohat
         self.state = 0
 
     def receive_signal(self, signal):
@@ -54,34 +50,27 @@ class mainPage(genericPage):
             pass
         elif signal == signal.SIGUSR2:
             print("K2 pressed")
-            pass
+            self.change_page(testPage)
         elif signal == signal.SIGALRM:
             print("K3 pressed")
-            pass
-        else:
-            pass
+            self.change_page(shutdownPage)
 
-    # perhaps this method should be in nanohat
-    def update_page(self, page):
-        # place lock
-        self.nanohat.page = page
-        # remove lock
-
-    def update_data(self):
-        self.ip_addr = get_ip()
-
-    def display_page(self):
-        self.update_data()
+    @if_free
+    def display(self):
+        self._update_data()
         text = "IP: {0}".format(self.ip_addr)
-        self.draw.text((self.padding, self.padding), text,  font=self.nanohat.font10b, fill=255)
+        self.draw.text((1, 1), text, font=font10b, fill=255)
         text = "ip"
-        self.draw.text((self.padding+3, self.padding+50), text,  font=self.nanohat.font10b, fill=255)
+        self.draw.text((4, 51), text, font=self.nanohat.font10b, fill=255)
         text = "test"
-        self.draw.text((self.padding+50, self.padding+50), text,  font=self.nanohat.font10b, fill=255)
+        self.draw.text((51, 51), text, font=self.nanohat.font10b, fill=255)
         text = "sd"
-        self.draw.text((self.padding+110, self.padding+50), text,  font=self.nanohat.font10b, fill=255)
+        self.draw.text((111, 51), text, font=self.nanohat.font10b, fill=255)
         oled.drawImage(self.nanohat.image)
         self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
+
+    def _update_data(self):
+        self.ip_addr = get_ip()
 
 
 class testPage(genericPage):
@@ -149,6 +138,7 @@ class testPage(genericPage):
         oled.drawImage(self.image)
         self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
 
+
 class shutdownPage(genericPage):
     """Represents the shutdown page, which takes care of shutting the
        system down."""
@@ -178,47 +168,14 @@ class shutdownPage(genericPage):
         os.system('sudo poweroff')
 
 
-class NanoHatOled(object):
-    """Object for interfacing to the nanohat's OLED display and buttons."""
-    
-    def __init__(self):
-        # create default page
-        self.width = 128
-        self.height = 64
-        self.padding = 1
-
-        self.iperf_duration = 10
-        self.iperf_server = '192.168.1.72'
-
-        self.image = Image.new('1', (self.width, self.height))
-        self.draw = ImageDraw.Draw(self.image)
-        self.font10b = ImageFont.truetype('DejaVuSansMono-Bold.ttf', 10)
-        self.font11 = ImageFont.truetype('DejaVuSansMono.ttf', 11)
-        self.font14 = ImageFont.truetype('DejaVuSansMono.ttf', 14)
-        self.font14b = ImageFont.truetype('DejaVuSansMono-Bold.ttf', 14)
-        self.font24b = ImageFont.truetype('DejaVuSansMono-Bold.ttf', 24)
-        oled.init()
-        oled.setNormalDisplay()      #Set display to normal mode (i.e non-inverse mode)
-        oled.setHorizontalMode()
-        self.telusSlogan()
-        time.sleep(2)
-        self.ipPage()
-        signal.signal(signal.SIGUSR1, self.receiveSignal) # button 1 (left)
-        signal.signal(signal.SIGUSR2, self.receiveSignal) # button 2 (middle)
-        signal.signal(signal.SIGALRM, self.receiveSignal) # button 3 (right)
-
-    def receiveSignal(self, signum, stack):
-        """Called whenever a signal is received."""
-        # start a new thread with page.
-
-
-def telusSlogan(self):
+def telusSlogan():
     """Prints the Telus slogan"""
-    self.draw.text((27, 1), "TELUS", font=self.font24b, fill=255)
-    self.draw.text((23, 31), "The Future", font=self.font14, fill=255)
-    self.draw.text((20, 45), "is Friendly", font=self.font14, fill=255)
-    oled.drawImage(self.image)
-    self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
+    draw.text((27, 1), "TELUS", font=font24b, fill=255)
+    draw.text((23, 31), "The Future", font=font14, fill=255)
+    draw.text((20, 45), "is Friendly", font=font14, fill=255)
+    oled.drawImage(image)
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
 
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -232,19 +189,58 @@ def get_ip():
         s.close()
     return IP
 
-def periodic_display(nanohat):
+
+def if_free(func):
+    """Made to be used as a decorator on functions that require locked to be free"""
+    if not locked:
+        locked = True
+        func(*args, **kwargs)
+        locked = False
+
+
+def periodic_display():
     while True:
-        # check if lock is applied, if not
-        # display
-        page.display_page()
-        # delay
+        page.display()
+        time.sleep(0.5)
+
+
+def receiveSignal(signum, stack):
+    """Called whenever a signal is received."""
+    threading.Thread(target=page.receive_signal, args=[signum])
 
 
 if __name__ == "__main__":
-    lock = False
+
+    # initialization of constants
+    width = 128
+    height = 64
+    padding = 1
+    iperf_duration = 10
+    iperf_server = '192.168.1.72'
+    locked = False
+
+    # initialization of display
+    image = Image.new('1', (width, height))
+    draw = ImageDraw.Draw(image)
+    font10b = ImageFont.truetype('DejaVuSansMono-Bold.ttf', 10)
+    font11 = ImageFont.truetype('DejaVuSansMono.ttf', 11)
+    font14 = ImageFont.truetype('DejaVuSansMono.ttf', 14)
+    font14b = ImageFont.truetype('DejaVuSansMono-Bold.ttf', 14)
+    font24b = ImageFont.truetype('DejaVuSansMono-Bold.ttf', 24)
+    oled.init()
+    oled.setNormalDisplay()      #Set display to normal mode (i.e non-inverse mode)
+    oled.setHorizontalMode()
+    telusSlogan()
+    time.sleep(2)
+
+    # assign receiveSignal to be called when buttons are pressed
+    signal.signal(signal.SIGUSR1, receiveSignal) # button 1 (left)
+    signal.signal(signal.SIGUSR2, receiveSignal) # button 2 (middle)
+    signal.signal(signal.SIGALRM, receiveSignal) # button 3 (right)
+
     page = mainPage()
-    nanohat = NanoHatOled()
     # spawn display thread
-    display_thread = threading.Thread(
+    threading.Thread(target=periodic_display)
+
     while True:
         signal.pause()
